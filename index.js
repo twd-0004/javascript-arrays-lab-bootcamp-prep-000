4d13676567bb9736467b8c23cb9879f339c9fc3e
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 00:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ function appendKitten(name) {
   var newKittens = [...kittens, name];
   return newKittens
 }
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens
+}
